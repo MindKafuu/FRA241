08f9d0e04fcf0dfc24a179ecfb955890a4ec00e5
Mind Kafuu <chanagan_mind@msn.com>
2017-10-27 11:11:44 +0700

    edit-user

---
+++ fra241-user/page5.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <!--meta-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"/>
+
+    <!--shortcut-->
+    <link rel="shortcut icon" href="images/icon.png" title="Favicon"/>
+
+    <!--style-->
+    <link rel="stylesheet" type="text/css" href="css/page5-1.css">
+    <link rel="stylesheet" type="text/css" href="css/page5-2.css">
+    <link rel="stylesheet" type="text/css" href="css/page5-3.css">
+
+    <!--font-->
+    <link href="https://fonts.googleapis.com/css?family=Anonymous+Pro|Work+Sans" rel="stylesheet">
+
+    <title>Classroom Schedule</title>
+  </head>
+  <style>
+    body {
+    background-color: #ececec;
+    color: #ff6c00 ;
+    font-family: 'Work Sans', sans-serif;
+    font-size: 1em;
+    text-align: left;}
+    header {
+       background: #ff6c00 none repeat-x 0 0;
+       color: #FFFFFF;
+       margin-top: 0px;
+       padding: 0 5px;
+       text-align: left;}
+  </style>
+  <body>
+    <header>
+        <img src="images/FIBO_logo.jpg" width="55" height="62" style="margin-top: 10px; margin-left: 10px">
+        <div style="font-size: 1.8em; margin-top: -50px; margin-left: 80px">
+        <b>Classroom Schedule</b>
+        </div><br>
+        </header>
+        <div style="margin-top: -30px; margin-left: 500px">
+        <a href="#" class="btn1"><b>Profile</b></a>
+        <a href="page4.php" class="btn1"><b>Inconvenient time</b></a>
+        <a href="page5.php" class="btn1" style=" color: #79a2ff"><b>Classroom</b></a>
+        <a href="page6.php" class="btn1"><b>Schedule</b></a>  
+        </div>
+        <div style="margin-top: -84px; margin-left: 1160px">
+            <a href="index.php" class="btn"><b>logout</b></a>
+        </div>
+        <div style="margin-top: 50px;">
+            <br><b class="topics">Classroom</b><br><br>
+            
+        </div>
+        <div style="margin-top: 20px; margin-left: 400px">
+        <p style =" font-family: 'Work Sans', sans-serif; font-size: 36px;"><b> Subject code  </b> </p> </br>
+        <p style =" font-family: 'Work Sans', sans-serif; font-size: 36px"><b>  Subject    </b>    </p></br>
+        <p style =" font-family: 'Work Sans', sans-serif; font-size: 36px"> <b> lecturer   </b>    </p></br>
+        <p style =" font-family: 'Work Sans', sans-serif; font-size: 36px"> <b> Hour/week   </b>   </p></br></br>
+        <div style = "margin-top: -275px; margin-left: 300px">
+                <input class = "in1"></br></br></br>
+                <input class = "in1"></br></br></br>
+                <input class = "in1"></br></br></br>
+                <input class = "in1"></br></br></br>
+        </div>
+          <b>
+            <ul id="navbar">
+                <li><a href="#"style ="background: url(images/arrow.png); 
+                    background-repeat: no-repeat; 
+                    background-size: 25px;
+                    background-position: right center;
+                    background-color:#ffffff">Sec</a>
+                    <ul class="item">
+                        <li><a href="#">A </a></li>
+                        <li><a href="#">B </a></li>
+                        <li><a href="#" class="forAB">A+B </a>
+                            <ul class="sub-item">
+                                <li><a href="#">same time</a></li>
+                                <li><a href="#">different time</a></li>
+                                
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li><a href="#" style ="background: url(images/arrow.png); 
+                    background-repeat: no-repeat; 
+                    background-size: 25px;
+                    background-position: right center;
+                    background-color:#ffffff">A</a>
+                    <ul class="item">
+                            <li><a href="#">computer </a></li>
+                            <li><a href="#">classroom</a></li>
+                            <li><a href="#">studio</a>
+                                
+                            </li>
+                        </ul>
+                </li>
+                <li><a href="#"style ="background: url(images/arrow.png); 
+                    background-repeat: no-repeat; 
+                    background-size: 25px;
+                    background-position: right center;
+                    background-color:#ffffff">B</a>
+                    <ul class="item">
+                        <li><a href="#">computer</a></li>
+                        <li><a href="#">classroom</a></li>
+                        <li><a href="#">studio </a></li>
+                        
+                            
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+            
+          </b>
+          <div style =" margin-left:800px; margin-top: 100px; font-size: 24px; font-family: 'Work Sans', sans-serif"><a href="#" class = "button3">
+                <span class = "content">SAVE</span> 
+            </a></div>
+  </body>
+</html>
